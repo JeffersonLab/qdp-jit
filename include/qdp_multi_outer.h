d0d57affac230f0dd9b9e066a5f6fc84ac201828
Frank Winter <fwinter@jlab.org>
2019-03-13 16:10:50 -0400

    Add multi4d specialization

--- include/qdp_multi_outer.h
+++ include/qdp_multi_outer.h
@@ -513,6 +513,15 @@ namespace QDP {
     
     explicit multi3d(int ns3, int ns2, int ns1) {resize(ns3,ns2,ns1);}
   
+    multi3d(OScalar<T1>* f, int ns3, int ns2, int ns1, int id_ )
+    {
+      F=f;
+      n1=ns1;
+      n2=ns2;
+      n3=ns3;
+      copymem=true;
+      id = id_;
+    }
 
     ~multi3d()
     {
@@ -678,6 +687,203 @@ namespace QDP {
     int n1 = 0;
     int n2 = 0;
     int n3 = 0;
+    int id = -1;
+    OScalar<T1>* F = NULL;
+  };
+
+
+
+
+
+
+
+
+
+  template<>
+  template<class T1> class multi4d< OScalar<T1> >
+  {
+  public:
+    typedef OScalar<T1> T;
+    
+    multi4d() {}
+    
+    explicit multi4d(int ns4, int ns3, int ns2, int ns1) {resize(ns4,ns3,ns2,ns1);}
+  
+    multi4d(OScalar<T1>* f, int ns4, int ns3, int ns2, int ns1, int id_ )
+    {
+      F=f;
+      n1=ns1;
+      n2=ns2;
+      n3=ns3;
+      n4=ns4;
+      copymem=true;
+      id = id_;
+    }
+
+    ~multi4d()
+    {
+      if (! copymem)
+	{
+	  delete[] F;
+	  if ( id > 0 )
+	    QDP_get_global_cache().signoff( id );
+	}
+    }
+
+    //! Copy constructor
+    multi4d(const multi4d& s)
+    {
+      resize(s.n4,s.n3,s.n2,s.n1);
+
+      for(int i=0; i < n1 * n2 * n3 *n4 ; ++i)
+	F[i] = s.F[i];
+    }
+
+    
+    void resize(int ns4, int ns3, int ns2 , int ns1) 
+    {
+      assert(!copymem);
+
+      //QDPIO::cout << "resize multi2d<OScalar> specialization\n";
+      if (id >= 0) {
+	QDP_get_global_cache().signoff( id );
+      }
+
+      n1 = ns1;
+      n2 = ns2;
+      n3 = ns3;
+      n4 = ns4;
+      int sz = n1 * n2 * n3 * n4;
+
+      id = QDP_get_global_cache().addArray( sizeof(T1) , sz );
+
+      delete[] F;
+      F = new(nothrow) OScalar<T1>[sz];
+      if ( F == 0x0 ) { 
+	QDP_error_exit("Unable to allocate memory in multi1d::resize(%d)\n",ns1);
+      }
+
+#if 1
+      // Initialize all elements as the appropriate array element
+      for ( int i = 0 ; i < sz ; ++i )
+	{
+	  //F[i].setElemNum( i + id_offset );
+	  F[i].setElemNum( i );
+	  F[i].setId(id);
+	}
+#endif
+    }
+
+    int getId() const {
+      return id;
+    }
+
+
+    //! Size of array
+    int size1() const {return n1;}
+    int size2() const {return n2;}
+    int size3() const {return n3;}
+    int size4() const {return n4;}
+
+    //! Equal operator uses underlying = of T
+    multi4d<T>& operator=(const multi4d<T>& s1)
+    {
+      resize(s1.size4(), s1.size3(), s1.size2(), s1.size1());
+
+      for(int i=0; i < n1*n2*n3*n4; ++i)
+	F[i] = s1.F[i];
+      return *this;
+    }
+
+#if 0
+    //! Return ref to a row slice
+    const T* slice(int k, int j) const {return F+n1*(j+n2*(k));}
+#endif
+
+    template<class T2>
+    multi4d< OScalar<T1> >& operator=(const T2& s1)
+    {
+      if (F == 0 || id < 0)
+      {
+	cerr << "multi4d: left hand side not initialized in =" << endl;
+	exit(1);
+      }
+
+      for(int i=0; i < n1*n2*n3*n4; ++i)
+	F[i] = s1;
+      return *this;
+    }
+
+    
+    //! Return ref to an element
+    //T& operator()(int k, int j, int i) {return F[i+n1*(j+n2*(k))];}
+
+    OScalar<T1>& operator()(int l, int k, int j, int i) {
+      assert( i >= 0 && i < n1 );
+      assert( j >= 0 && j < n2 );
+      assert( k >= 0 && k < n3 );
+      assert( l >= 0 && l < n4 );
+      //F[i].setElemNum( i + n1 * ( j + n2 * k ) );
+      //F[i].setId( id );
+      //return F[i];
+      return F[i + n1 * ( j + n2 * ( k + n3 * l ) )];
+    }
+
+  
+    //! Return const ref to an element
+    //
+
+    const OScalar<T1>& operator()(int l, int k, int j, int i) const {
+      assert( i >= 0 && i < n1 );
+      assert( j >= 0 && j < n2 );
+      assert( k >= 0 && k < n3 );
+      assert( l >= 0 && l < n4 );
+      //F[i].setElemNum( i + n1 * ( j + n2 * k ) );
+      //F[i].setId( id );
+      //return F[i];
+      return F[i + n1 * ( j + n2 * ( k + n3 * l ) )];
+    }
+
+
+    // //! Return ref to an element
+    // multi1d<T> operator[](int j) {return multi1d<T>(F+j*n1,n1);}
+
+    // multi1d< OScalar<T1> > operator[](int j) {
+    //   assert( j >= 0 && j < n2 );
+    //   return multi1d< OScalar<T1> >( F + j * n1 , n1 , id , j * n1 );
+    // }
+
+    
+    //multi2d<T> operator[](int k) {return multi2d<T>( F+n1*n2*k ,n2,n1);}
+
+    multi3d< OScalar<T1> > operator[](int l) {
+      assert( l >= 0 && l < n4 );
+      //return multi2d< OScalar<T1> >( F + n1 * n2 * k , n2 , n1 , id , n1 * n2 * k );
+      return multi3d< OScalar<T1> >( F + n1 * n2 * n3 * l , n3 , n2 , n1 , id );
+    }
+
+  
+    //! Return const ref to an element
+    //const multi1d<T> operator[](int j) const {return multi1d<T>(F+j*n1,n1);}
+
+    const multi3d< OScalar<T1> > operator[](int l) const {
+      assert( l >= 0 && l < n4 );
+      //return multi2d< OScalar<T1> >( F + n1 * n2 * k , n2 , n1 , id , n1 * n2 * k );
+      return multi3d< OScalar<T1> >( F + n1 * n2 * n3 * l , n3 , n2 , n1 , id );
+    }
+
+    // const multi1d< OScalar<T1> > operator[](int j) const {
+    //   assert( j >= 0 && j < n2 );
+    //   return multi1d< OScalar<T1> >( F + j * n1 , n1 , id , j * n1 );
+    // }
+
+  
+  private:
+    bool copymem = false;
+    int n1 = 0;
+    int n2 = 0;
+    int n3 = 0;
+    int n4 = 0;
     int id = -1;
     OScalar<T1>* F = NULL;
   };
