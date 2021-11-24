834b65d4e573b6f9f1c2fc3011584ece3355abcb
Frank Winter <fwinter@jlab.org>
2021-11-24 12:38:12 -0500

    Fix for CUDA/HIP managed memory
    For now only CUDA.
    QDP_ENABLE_MANAGED_MEMORY=ON/OFF
    
    This mode (managed/unmanaged) is used for the
    pool's memory (one big allocation) and the rest.
    
    GPU memory allocations either go to the pool
    or directly to the GPU API, decided by
    
    -pool-max-alloc <N>
    
    where the default N=-1 means the maximum allocation
    is infinite and all allocations go to the pool.
    
    Setting N=0 and thus directing all allocations to
    the native GPU API has performance impact as there
    are frequent small allocations. Can alleviate by
    setting N some small positive number and using
    a poolsize of only a fraction of GPU memory. Sweet
    spot searching.
    
    Cleaner, long term solution would be a cache for
    GPU allocations. Probably acting only on small
    allocations.

--- lib/qdp_gpu_cuda.cc
+++ lib/qdp_gpu_cuda.cc
@@ -504,7 +504,7 @@ namespace QDP {
 if (size == 0) *mem = nullptr;
     CUresult ret;
     CudaCheckResult(cuCtxSetCurrent(cuContext));
-#ifndef QDP_USE_CUDA_MANAGED_MEMORY
+#ifndef QDP_ENABLE_MANAGED_MEMORY
     CudaCheckResult(cudaMalloc(mem, size));
 #else
     CudaCheckResult(cudaMallocManaged(mem, size, cudaMemAttachGlobal));
@@ -514,7 +514,7 @@ if (size == 0) *mem = nullptr;
 #else
 
     CUresult ret;
-#ifndef QDP_USE_CUDA_MANAGED_MEMORY
+#ifndef QDP_ENABLE_MANAGED_MEMORY
     ret = cuMemAlloc( (CUdeviceptr*)mem,size);
 #else
     ret = cuMemAllocManaged( (CUdeviceptr*)mem, size, CU_MEM_ATTACH_GLOBAL ); 
