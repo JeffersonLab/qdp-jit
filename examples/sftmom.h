013c5efa060c1fac69601a9a187565821f84795c
Frank Winter <fwinter@jlab.org>
2022-07-24 15:18:33 -0400

    Examples: add t_mesons

---
+++ examples/sftmom.h
@@ -0,0 +1,119 @@
+// -*- C++ -*-
+//  $Id: sftmom.h,v 3.9 2009-02-17 16:33:38 edwards Exp $
+/*! \file
+ *  \brief Fourier transform phase factor support
+ */
+
+#ifndef __sftmom_h__
+#define __sftmom_h__
+
+//#include "chromabase.h"
+#include "qdp.h"
+
+
+
+
+  //! Fourier transform phase factor support
+  /*!
+   * \ingroup ft
+   */
+  class SftMom
+  {
+  public:
+    //! Constructor about origin
+    SftMom(int mom2_max, bool avg_equiv_mom_=false, int j_decay=-1);
+
+#if 0
+    //! Constructor about origin, with a list of momenta 
+    SftMom(const multi2d<int> & moms , int j_decay=-1);
+#endif
+    
+    //! Construct around some fixed origin_offset
+    SftMom(int mom2_max, multi1d<int> origin_offset_,
+	   bool avg_equiv_mom_=false, int j_decay=-1) ;
+
+    //! Construct around some fixed origin_offset and mom_offset
+    SftMom(int mom2_max, multi1d<int> origin_offset_, multi1d<int> mom_offset_,
+	   bool avg_equiv_mom_=false, int j_decay=-1) 
+      { init(mom2_max, origin_offset_, mom_offset_, avg_equiv_mom_, j_decay); }
+
+
+    //! The set to be used in sumMulti
+    const Set& getSet() const { return sft_set; }
+
+    //! Number of momenta
+    int numMom() const { return num_mom; }
+
+    //! Number of subsets - length in decay direction
+    int numSubsets() const { return sft_set.numSubsets(); }
+
+    //! Number of sites in each subset
+    size_t numSites() const;
+
+    //! Decay direction
+    int getDir() const { return decay_dir; }
+
+    //! Are momenta averaged?
+    bool getAvg() const { return avg_equiv_mom; }
+
+    //! Momentum offset
+    multi1d<int> getMomOffset() const { return mom_offset; }
+
+    //! Convert momenta id to actual array of momenta
+    multi1d<int> numToMom(int mom_num) const { return mom_list[mom_num]; }
+
+    //! Convert array of momenta to momenta id
+    /*! \return id in [0,numMom()-1] or -1 if not in list */
+    int momToNum(const multi1d<int>& mom_in) const;
+
+    //! Canonically order an array of momenta
+    /*! \return abs(mom[0]) >= abs(mom[1]) >= ... >= abs(mom[mu]) >= ... >= 0 */
+    multi1d<int> canonicalOrder(const multi1d<int>& mom) const;
+
+    //! Return the phase for this particular momenta id
+    const LatticeComplex& operator[](int mom_num) const
+      { return phases[mom_num]; }
+
+    //! Return the the multiplicity for this momenta id.
+    /*! Only nonzero if momentum averaging is turned on */
+    int multiplicity(int mom_num) const
+      { return mom_degen[mom_num]; }
+
+    //! Do a sumMulti(cf*phases,getSet())
+    multi2d<DComplex> sft(const LatticeComplex& cf) const;
+
+    //! Do a sum(cf*phases,getSet()[my_subset])
+    multi2d<DComplex> sft(const LatticeComplex& cf, int subset_color) const;
+
+    //! Do a sumMulti(cf*phases,getSet())
+    multi2d<DComplex> sft(const LatticeReal& cf) const;
+
+    //! Do a sumMulti(cf*phases,getSet()[my_subset])
+    multi2d<DComplex> sft(const LatticeReal& cf, int subset_color) const;
+
+#if BASE_PRECISION==32
+    multi2d<DComplex> sft(const LatticeComplexD& cf) const;
+    //! Do a sum(cf*phases,getSet()[my_subset])
+    multi2d<DComplex> sft(const LatticeComplexD& cf, int subset_color) const;
+#endif
+
+  private:
+    SftMom() {} // hide default constructor
+
+    void init(int mom2_max, multi1d<int> origin_offset, multi1d<int> mom_offset,
+	      bool avg_mom_=false, int j_decay=-1);
+
+    multi2d<int> mom_list;
+    bool         avg_equiv_mom;
+    int          decay_dir;
+    int          num_mom;
+    multi1d<int> origin_offset;
+    multi1d<int> mom_offset;
+    multi1d<LatticeComplex> phases;
+    multi1d<int> mom_degen;
+    Set sft_set;
+  };
+
+
+
+#endif
---
+++ examples/t_mesons.cc
@@ -0,0 +1,167 @@
+// $Id: t_dslashm.cc,v 1.20 2007-02-24 01:00:29 bjoo Exp $
+/*! \file
+ *  \brief Test the Wilson-Dirac operator (dslash)
+ */
+
+#include "qdp.h"
+#include "sftmom.h"
+//#include "examples.h"
+//#include <iostream>
+//#include <cstdio>
+//#include <sys/time.h>
+
+using namespace QDP;
+
+
+
+
+
+
+void mesons2(const LatticePropagator& quark_prop_1,
+	     const LatticePropagator& quark_prop_2,
+	     const SftMom& phases,
+	     int t0 )
+{
+  StopWatch wall;
+  wall.start();
+
+  // Length of lattice in decay direction
+  int length = phases.numSubsets();
+
+  // Construct the anti-quark propagator from quark_prop_2
+  int G5 = Ns*Ns-1;
+  LatticePropagator anti_quark_prop =  Gamma(G5) * quark_prop_2 * Gamma(G5);
+
+  // This variant uses the function SftMom::sft() to do all the work
+  // computing the Fourier transform of the meson correlation function
+  // inside the class SftMom where all the of the Fourier phases and
+  // momenta are stored.  It's primary disadvantage is that it
+  // requires more memory because it does all of the Fourier transforms
+  // at the same time.
+
+
+  
+  for (int gamma_value=0; gamma_value < 1; ++gamma_value)
+  {
+    QDPIO::cout << "gamma value = " << gamma_value << std::endl;
+
+    LatticeComplex corr_fn;
+
+    corr_fn = trace(adj(anti_quark_prop) * (Gamma(gamma_value) *
+					    quark_prop_1 * Gamma(gamma_value)));
+
+    StopWatch w;
+    w.start();
+
+#if 1
+    corr_fn = trace(adj(anti_quark_prop) * (Gamma(gamma_value) *
+					    quark_prop_1 * Gamma(gamma_value)));
+#else
+    LatticePropagator tmp = Gamma(gamma_value) * quark_prop_1;
+    LatticePropagator tmp2 = tmp * Gamma(gamma_value);
+    corr_fn = trace(adj(anti_quark_prop) * tmp2);
+#endif
+    
+    //LatticePropagator tmp = Gamma(2) * anti_quark_prop;
+    
+    w.stop();
+    double t_corr = w.getTimeInSeconds();
+    QDPIO::cout << "time trace = " << t_corr << std::endl;
+
+    w.reset();
+    w.start();
+    
+    multi2d<DComplex> hsum;
+    hsum = phases.sft(corr_fn);
+
+    w.stop();
+    double t_phases = w.getTimeInSeconds();
+    QDPIO::cout << "time phases = " << t_phases << std::endl;
+
+#if 0
+    for (int sink_mom_num=0; sink_mom_num < phases.numMom(); ++sink_mom_num) 
+    {
+      multi1d<DComplex> mesprop(length);
+      for (int t=0; t < length; ++t) 
+      {
+        int t_eff = (t - t0 + length) % length;
+	mesprop[t_eff] = hsum[sink_mom_num][t];
+      }
+    } // end for(sink_mom_num)
+#endif
+
+    
+  } // end for(gamma_value)
+
+  wall.stop();
+  double t_wall = wall.getTimeInSeconds();
+  QDPIO::cout << "time overall = " << t_wall << std::endl;
+
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  // Put the machine into a known state
+  QDP_initialize(&argc, &argv);
+
+  // Setup the layout
+  const int foo[] = {16,16,16,16};
+  multi1d<int> nrow(Nd);
+  nrow = foo;  // Use only Nd elements
+
+  int mom2_max = 1;
+  int j_decay = Nd-1;
+  
+  for (int i=1; i<argc; i++) 
+    {
+      if (strcmp((argv)[i], "-lat")==0) 
+	{
+	  int lat;
+	  sscanf((argv)[++i], "%d", &lat);
+	  nrow[0]=nrow[1]=nrow[2]=nrow[3]=lat;
+	}
+      if (strcmp((argv)[i], "-latz")==0) 
+	{
+	  int lat;
+	  sscanf((argv)[++i], "%d", &lat);
+	  nrow[3]=lat;
+	}
+      if (strcmp((argv)[i], "-mom2_max")==0) 
+	{
+	  int lat;
+	  sscanf((argv)[++i], "%d", &lat);
+	  mom2_max=lat;
+	}
+
+    }
+
+  Layout::setLattSize(nrow);
+  Layout::create();
+
+  //! Test out propagators
+  multi1d<LatticeColorMatrix> u(Nd);
+  for(int m=0; m < u.size(); ++m)
+    gaussian(u[m]);
+
+  SftMom sf(mom2_max, false, j_decay);
+
+  QDPIO::cout << "number of subsets: " << sf.numSubsets() << std::endl;
+  QDPIO::cout << "number of momenta: " << sf.numMom() << std::endl;
+
+  LatticePropagator quark_prop_1;
+  LatticePropagator quark_prop_2;
+
+  QDPIO::cout << "Gaussian noise prop 1" << std::endl;
+  gaussian(quark_prop_1);
+  QDPIO::cout << "Gaussian noise prop 2" << std::endl;
+  gaussian(quark_prop_2);
+
+  mesons2( quark_prop_1 , quark_prop_2 , sf , 0 );
+  
+  // Time to bolt
+  QDP_finalize();
+}
