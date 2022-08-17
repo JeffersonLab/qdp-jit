9c8bd4c6c937bf101cbe864f94d91753bf7ab91d
Frank Winter <fwinter@jlab.org>
2022-08-17 16:48:48 -0400

    Adds support for sums of expressions

--- include/qdp_jitf_sum.h
+++ include/qdp_jitf_sum.h
@@ -60,122 +60,12 @@ namespace QDP {
   
 
 
-  // T1 input
-  // T2 output
-  template< class T1 , class T2 , JitDeviceLayout input_layout >
-  void
-  function_sum_convert_ind_build(JitFunction& function)
-  {
-#ifdef QDP_DEEP_LOG
-    function.type_W = typeid(typename WordType<T2>::Type_t).name();
-#endif
-
-    llvm_start_new_function("sum_convert_ind",__PRETTY_FUNCTION__ );
-
-    ParamRef p_lo     = llvm_add_param<int>();
-    ParamRef p_hi     = llvm_add_param<int>();
-
-    typedef typename WordType<T1>::Type_t T1WT;
-    typedef typename WordType<T2>::Type_t T2WT;
-
-    ParamRef p_site_perm  = llvm_add_param< int* >(); // Siteperm  array
-    ParamRef p_idata      = llvm_add_param< T1WT* >();  // Input  array
-    ParamRef p_odata      = llvm_add_param< T2WT* >();  // output array
-
-    OLatticeJIT<typename JITType< typename ScalarType<T1>::Type_t >::Type_t> idata(  p_idata );   // want coal   access later
-    OLatticeJIT<typename JITType<                     T2>::Type_t          > odata(  p_odata );   // want scalar access later
-
-    llvm_derefParam( p_lo ); // r_lo
-    llvm::Value* r_hi     = llvm_derefParam( p_hi );
-
-    //llvm_derefParam( p_idata );  // Input  array
-    //llvm_derefParam( p_odata );  // output array
-
-    llvm::Value* r_shared = llvm_get_shared_ptr( llvm_get_type<T2WT>() , gpu_getMaxSMem() / sizeof(T2WT) );
-
-    typedef typename JITType<T2>::Type_t T2JIT;
-
-    llvm::Value* r_idx = llvm_thread_idx();
-
-    llvm::Value* r_block_idx  = llvm_call_special_ctaidx();
-    llvm::Value* r_tidx       = llvm_call_special_tidx();
-    llvm::Value* r_ntidx       = llvm_call_special_ntidx(); // this is a power of 2
-
-    IndexDomainVector args;
-    args.push_back( make_pair( Layout::sitesOnNode() , r_tidx ) );  // sitesOnNode irrelevant since Scalar access later
-    T2JIT sdata_jit;
-    sdata_jit.setup( r_shared , JitDeviceLayout::Scalar , args );
-    zero_rep( sdata_jit );
-
-
-    JitIf ifValidThreadIdx( llvm_lt( r_idx , r_hi ) );
-    {
-      llvm::Value* r_idx_perm = llvm_array_type_indirection( p_site_perm , r_idx );
-
-      typename REGType< typename JITType< typename ScalarType<T1>::Type_t >::Type_t >::Type_t reg_idata_elem;   
-      reg_idata_elem.setup( idata.elem( input_layout , r_idx_perm ) );
-
-      sdata_jit = reg_idata_elem; // This should do the precision conversion (SP->DP)
-    }
-    ifValidThreadIdx.end();
-
-    llvm_bar_sync();
-
-    
-    llvm::Value* r_pow_shr1 = llvm_shr( r_ntidx , llvm_create_value(1) );
-
-    //
-    // Reduction loop
-    //
-    JitForLoopPower loop( r_pow_shr1 );
-    {
-      JitIf ifInRange( llvm_lt( r_tidx , loop.index() ) );
-      {
-	llvm::Value * v = llvm_add( loop.index() , r_tidx );
-
-	JitIf ifInRange2( llvm_lt( v , r_ntidx ) );
-	{
-	  IndexDomainVector args_new;
-	  args_new.push_back( make_pair( Layout::sitesOnNode() , 
-					 llvm_add( r_tidx , loop.index() ) ) );  // sitesOnNode irrelevant since Scalar access later
-
-	  typename JITType<T2>::Type_t sdata_jit_plus;
-	  sdata_jit_plus.setup( r_shared , JitDeviceLayout::Scalar , args_new );
-
-	  typename REGType< typename JITType<T2>::Type_t >::Type_t sdata_reg_plus;
-	  sdata_reg_plus.setup( sdata_jit_plus );
-
-	  sdata_jit += sdata_reg_plus;
-	}
-	ifInRange2.end();
-      }
-      ifInRange.end();
-
-      llvm_bar_sync();
-    }
-    loop.end();
-    //
-    // -------------------
-    //
-
-    JitIf ifStore( llvm_eq( r_tidx , llvm_create_value(0) ) );
-    {
-      typename REGType< typename JITType<T2>::Type_t >::Type_t sdata_reg;   
-      sdata_reg.setup( sdata_jit );
-      odata.elem( JitDeviceLayout::Scalar , r_block_idx ) = sdata_reg;
-    }
-    ifStore.end();
-
-    jit_get_function(function);
-  }
-
-
 
 
   // T1 input
-  template< JitDeviceLayout input_layout, class T1 , class RHS >
+  template< class T1 , class RHS >
   void
-  function_sum_convert_ind_expr_build( JitFunction& function , const QDPExpr<RHS,OLattice<T1> >& rhs )
+  function_sum_convert_ind_expr_build( JitFunction& function , const QDPExpr<RHS,OLattice<T1> >& rhs , JitDeviceLayout input_layout )
   {
     typedef typename UnaryReturn< OLattice<T1> , FnSum>::Type_t::SubType_t T2;
 
