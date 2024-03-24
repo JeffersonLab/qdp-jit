f6f2815f1e607f01c4d9a5d3b5d0232e94a9d4c2
Frank Winter <fwinter@jlab.org>
2024-03-24 11:07:23 -0400

    ROCM: fix typo
    Added libomptarget-old-amdgpu as possible library

--- lib/qdp_llvm.cc
+++ lib/qdp_llvm.cc
@@ -242,7 +242,7 @@ namespace QDP
 #ifdef QDP_BACKEND_ROCM
     std::vector<std::string> vec_str_libdevice_path = { ROCM_DIR };
     std::vector<std::string> vec_str_libdevice_path_append = { "llvm/lib/libdevice/" , "llvm/lib/" };
-    std::vector<std::string> vec_str_libdevice_name = { "libm-amdgcn-ARCH.bc" , "libomptarget-amdgcn-ARCH.bc" , "libomptarget-new-amdpu-ARCH.bc" };
+    std::vector<std::string> vec_str_libdevice_name = { "libm-amdgcn-ARCH.bc" , "libomptarget-amdgcn-ARCH.bc" , "libomptarget-new-amdgpu-ARCH.bc" , "libomptarget-old-amdgpu-ARCH.bc" };
 #elif QDP_BACKEND_CUDA
     std::vector<std::string> vec_str_libdevice_path = { "CUDAPATH" , "/usr/local/cuda/" , "/usr/lib/nvidia-cuda-toolkit/" };
     std::vector<std::string> vec_str_libdevice_path_append = { "nvvm/libdevice/" , "libdevice/" , "cuda/nvvm/libdevice/" , "cuda/CUDAVERSION/nvvm/libdevice/"};
